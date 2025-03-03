366fa4cd14cbdbd38b541d52b019743f254defff
abdo389 <001abo001@gmail.com>
2025-03-03 16:10:32 +0200

    ;)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LoginPage from "./Pages/LoginPage/LoginPage";
+import LoginPage from "./Pages_temp/LoginPage/LoginPage";
 import GuestRoute from "./Components/GuestRoute/GuestRoute";
 import Layout from "./Components/Layout/Layout";
-import Home from "./Pages/Home";
-import RegisterPage from "./Pages/RegisterPage/RegisterPage";
-import ProductsPage from "./Pages/ProductsPage";
+import Home from "./Pages_temp/Home";
+import RegisterPage from "./Pages_temp/RegisterPage/RegisterPage";
+import ProductsPage from "./Pages_temp/ProductsPage";
 import Cart from "./Components/Cart/Cart";
-import FilteredProducts from "./Pages/FilteredProducts";
-import ProductDetails from "./Pages/ProductDetails";
-import ErrorPage from "./Pages/ErrorPage";
-import Orders from "./pages/Orders/Orders";
+import FilteredProducts from "./Pages_temp/FilteredProducts";
+import ProductDetails from "./Pages_temp/ProductDetails";
+import ErrorPage from "./Pages_temp/ErrorPage";
+import Orders from "./Pages_temp/Orders/Orders";
 import UserProvider from "./context/User.context";
 import HomeContextProvider from "./context/HomeContext";
 import CartContextProvider from "./context/CartContext";
 import ProductsContextProvider from "./context/ProductsContext";
-import OrderMethodPage from "./Pages/Orders/OrderMethodPage";
+import OrderMethodPage from "./Pages_temp/Orders/OrderMethodPage";
 import { Toaster } from "react-hot-toast";
 
 function App() {
